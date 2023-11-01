98f325232e7dd81a244a1078d2b26690680395f3
josephCD <josephdeabreudiaz@gmail.com>
2023-11-01 19:13:01 -0400

    Lectura de archivo txt

--- proyecto1.js
+++ proyecto1.js
@@ -1,1 +1,13 @@
-console.log("Hola giasdsadt");
+const fs = require('fs');
+
+// Nombre del archivo que quieres leer
+const nombreArchivo = 'archivo.txt';
+
+// Leer el archivo de texto
+fs.readFile(nombreArchivo, 'utf8', (error, data) => {
+    if (error) {
+        console.error('Error al leer el archivo:', error);
+        return;
+    }
+    console.log('Contenido del archivo:', data);
+});

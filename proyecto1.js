1c0396460d7bb55d88cffe99ff27b6b62269112b
josephCD <josephdeabreudiaz@gmail.com>
2023-11-01 19:21:23 -0400

    Lectura por linea

--- proyecto1.js
+++ proyecto1.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
 
-// Nombre del archivo que quieres leer
 const nombreArchivo = 'archivo.txt';
 
-// Leer el archivo de texto
+function analizarExpresionAritmetica(expresion) {
+    try {
+        // Intenta evaluar la expresión
+        eval(expresion);
+        console.log('Expresión válida');
+    } catch (error) {
+        console.error(`Error al evaluar la expresión: ${error.message}`);
+    }
+}
+
+// ... Código para leer el archivo ...
+
+// Dentro de la función de callback de fs.readFile
 fs.readFile(nombreArchivo, 'utf8', (error, data) => {
     if (error) {
         console.error('Error al leer el archivo:', error);
         return;
     }
-    console.log('Contenido del archivo:', data);
+
+    let lineas = data.split('\n');
+    for (let i = 0; i < lineas.length; i++) {
+        let expresion = lineas[i].trim();
+        console.log(`LINEA ${i + 1}: ${expresion}`);
+        analizarExpresionAritmetica(expresion);
+    }
 });

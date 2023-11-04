1d9b8a142056cb10f3b7a391d9e18c54c405132a
josephCD <josephdeabreudiaz@gmail.com>
2023-11-04 15:42:23 -0400

    V1.0 - Programa funcional con potencias y funcion

--- proyecto1.js
+++ proyecto1.js
@@ -4,6 +4,10 @@ function esDigito(caracter) {
   return /[0-9]/.test(caracter);
 }
 
+function esLetra(caracter) {
+  return /[a-zA-Z]/.test(caracter);
+}
+
 function analizadorLexico(expresion) {
   let tokens = [];
   let i = 0;
@@ -16,10 +20,19 @@ function analizadorLexico(expresion) {
         i++;
       }
       tokens.push({ tipo: 'NUMERO', valor: numero });
-    } else if (expresion[i] === '+' || expresion[i] === '-' || expresion[i] === '*' || expresion[i] === '/' || expresion[i] === '(' || expresion[i] === ')' || expresion[i] === ' ' || expresion[i] === '\n') {
-      // Agregar el salto de línea como caracter válido
+    } else if (esLetra(expresion[i])) {
+      let variable = '';
+      while (i < expresion.length && esLetra(expresion[i])) {
+        variable += expresion[i];
+        i++;
+      }
+      if (variable === 'sqrt' || variable === 'e' || variable === 'ln' || variable === 'sen' || variable === 'cos' || variable === 'tan' || variable === 'arcsen' || variable === 'arccos' || variable === 'arctan'){
+        tokens.push({ tipo: 'FUNCION', valor: variable });
+      } else {
+        tokens.push({ tipo: 'VARIABLE', valor: variable });
+      }
+    } else if (expresion[i] === '+' || expresion[i] === '-' || expresion[i] === '*' || expresion[i] === '/' || expresion[i] === '(' || expresion[i] === ')' || expresion[i] === '^' || expresion[i] === ' ' || expresion[i] === '\n') {
       if (expresion[i] !== ' ' && expresion[i] !== '\n') {
-        // Ignorar el espacio y el salto de línea
         tokens.push({ tipo: 'OPERADOR', valor: expresion[i] });
       }
       i++;
@@ -33,6 +46,9 @@ function analizadorLexico(expresion) {
 
 function analizadorSintactico(tokens) {
   let i = 0;
+  if (tokens.length === 0) {
+    throw new Error('Error de sintaxis. Expresion incompleta.');
+  }
 
   function expresion() {
     let termino1 = termino();
@@ -44,16 +60,30 @@ function analizadorSintactico(tokens) {
 
   function termino() {
     let factor1 = factor();
-    while (i < tokens.length && (tokens[i].valor === '*' || tokens[i].valor === '/')) {
+    while (i < tokens.length && (tokens[i].valor === '*' || tokens[i].valor === '/' || tokens[i].valor === '^')) {
       i++;
       let factor2 = factor();
     }
   }
 
   function factor() {
-    if (tokens[i].tipo === 'NUMERO') {
+    if (tokens[i].tipo === 'NUMERO' || tokens[i].tipo === 'VARIABLE') {
       i++;
-    } else if (tokens[i].valor === '(') {
+    } else if (tokens[i].tipo === 'FUNCION') {
+      // Verificar si hay un paréntesis de apertura '(' después del token de función
+      if (i + 1 < tokens.length && tokens[i + 1].valor === '(') {
+        i += 2; // Avanzar dos tokens para saltar el token de función y el '('
+        expresion();
+        // Verificar si hay un paréntesis de cierre ')' después de la expresión
+        if (i < tokens.length && tokens[i].valor === ')') {
+          i++; // Avanzar para saltar el ')'
+        } else {
+          throw new Error('Error de sintaxis. Falta paréntesis de cierre.');
+        }
+      } else {
+        throw new Error('Error de sintaxis. Se esperaba un paréntesis de apertura.');
+      }
+    }else if (tokens[i].valor === '(') {
       i++;
       expresion();
       if (tokens[i].valor === ')') {
@@ -87,9 +117,9 @@ for (let i = 0; i < lineas.length; i++) {
     analizadorSintactico(tokens);
 
     // La expresión es válida
-    fs.appendFileSync('salida.txt', `La expresión ${i + 1} es válida.\n`);
+    fs.appendFileSync('salida.txt', `LINEA ${i + 1}: SINTAXIS OK.\n`);
   } catch (error) {
     // La expresión es inválida
-    fs.appendFileSync('salida.txt', `Error en la expresión ${i + 1}: ${error.message}\n`);
+    fs.appendFileSync('salida.txt', `LINEA ${i + 1}: Error en la expresión (${error.message})\n`);
   }
 }
